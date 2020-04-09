a6e0a1648220b91edfedcfea07df8daaaa2ca9fb
Sholde <exshode@gmail.com>
2020-04-09 18:14:38 +0200

    reste a utiliser un a aleatoire

--- src/main.c
+++ src/main.c
@@ -11,35 +11,55 @@ int popcount(int n) {
     return pc;
 }
 
-int nb_bit(int n) {
-    int res = 32;
-    int masque = 0x80000000;
-    while( !(masque & n) ) {
-        masque >>= 1;
-        res--;
+void nb_bit(mpz_t nb, mpz_t h) {
+    mpz_set_ui(nb, 1);
+    while( mpz_div_ui(h, h, 2) && mpz_cmp_ui(h, 0) > 0 ) {
+        mpz_add_ui(nb, nb, 1);
     }
-    return res;
 }
 
-int pos(int n) {
-    int res = 1;
-    for(int i = 0; i < n; i++) {
-        res <<= 1;
+void pos(mpz_t test, mpz_t i) {
+    mpz_set_ui(test, 1);
+
+    mpz_t j;
+    mpz_init(j);
+    for(mpz_set_ui(j, 0); mpz_cmp(j, i) < 0; mpz_add_ui(j, j, 1)) {
+        mpz_mul_ui(test, test, 2);
     }
-    return res;
+    mpz_clear(j);
 }
 
 // a^h mod n
-int square_and_multiply(int a, int n, int h) {
-    int r = a;
-    int nb = nb_bit(h);
-    for(int i = nb - 1; i >= 0; i--) {
-        r = (r * r) % n;
-        if(h & pos(i)) {
-            r = (r * a) % n;
+void square_and_multiply(mpz_t res, mpz_t a, mpz_t n, mpz_t h) {
+    mpz_set(res, a);
+
+    mpz_t nb;
+    mpz_init(nb);
+    nb_bit(nb, h);
+
+    mpz_t i;
+    mpz_init(i);
+    mpz_set(i, nb);
+    mpz_sub_ui(i, i, 1);
+
+    mpz_t test;
+    mpz_init(test);
+
+    for(; mpz_cmp_ui(i, 0) >= 0; mpz_sub_ui(i, i, 1)) {
+        mpz_mul(res, res, res);
+        mpz_mod(res, res, n);
+
+        pos(test, i);
+        mpz_and(test, test, h);
+        if(mpz_cmp_ui(test, 0) != 0) {
+            mpz_mul(res, res, a);
+            mpz_mod(res, res, n);
         }
     }
-    return r;
+
+    mpz_clear(nb);
+    mpz_clear(i);
+    mpz_clear(test);
 }
 
 void decompose(mpz_t n, mpz_t s, mpz_t t) {
@@ -129,15 +149,43 @@ int miller_rabbin(mpz_t n, mpz_t k) {
     mpz_t y;
     mpz_init(y);
 
-    for(mpz_set_ui(i, 0); mpz_cmp(i, k) < 0; mpz_add_ui(i, i, 1)) {
+    mpz_t j;
+    mpz_init(j);
 
+    mpz_t deux;
+    mpz_init(deux);
+    mpz_set_ui(deux, 2);
+
+    int bk = 0;
+
+    for(mpz_set_ui(i, 0); mpz_cmp(i, k) < 0; mpz_add_ui(i, i, 1)) {
+        mpz_set(a, i);
+        square_and_multiply(y, a, t, n);
+        if( mpz_cmp_ui(y, 1) != 0 || mpz_cmp_ui(y, -1) != 0) {
+            for(mpz_set_ui(j, 1); mpz_cmp(j, s) < -1; mpz_add_ui(j, j, 1)) {
+                square_and_multiply(y, y, deux, n);
+                if( mpz_cmp_ui(y, 1) == 0 ) {
+                    return 0;
+                }
+                if( mpz_cmp_ui(y, -1) == 0 ) {
+                    bk = 1;
+                    break;
+                }
+            }
+            if( !bk ) {
+                return 0;
+            }
+            bk = 0;
+        }
     }
 
     mpz_clear(s);
     mpz_clear(t);
     mpz_clear(i);
+    mpz_clear(j);
     mpz_clear(a);
     mpz_clear(y);
+    mpz_clear(deux);
 
     return 1;
 }
