d743a36c9863261bf24fc39eb1b07eb7df77b620
LyndaHadjab <55532663+LyndaHadjab@users.noreply.github.com>
2020-04-13 15:22:29 +0200

    Update combine.c

--- combine.c
+++ combine.c
@@ -256,7 +256,7 @@ void Menu()
 {
 	printf("*************************DM CRYPTO***********************\n");
 	printf("Veuillez choisir une option :\n");
-	printf("\t 1-Exécuter un fichier  \n");
+	printf("\t 1-Exécuter un nombre d'un fichier  \n");
 	printf("\t 2-Entrer le nombre directement \n");
 	printf("\t 2-Quitter\n");
 	
