e4e934383472737123504a263193a93f87156204
Sholde <exshode@gmail.com>
2020-04-14 20:05:47 +0200

    correction fautes orthographe et explication signature

--- src/interface.h
+++ src/interface.h
@@ -1,19 +1,43 @@
 #ifndef _interface_h
 #define _interface_h
 
+/**
+ * Permet de lire un fichier contenant un nombre
+ * @param chemin : chemin du fichier à partir de l'exécution du programme
+ * @return chaîne de caractère contenant le nombre
+ */
 char *lire(char *chemin);
 
+/**
+ * Affiche le menu principale
+ */
 void menu(void);
 
+/**
+ * Affiche le menu des différent test de primalité possible
+ */
 void test_primal(void);
 
+/**
+ * Permet à l'utilisateur de choisir le nombre à tester
+ * @param n : le nombre à tester
+ */
 void choisis_nombre(mpz_t n);
 
+/**
+ * Permet à l'utilisateur de choisir le nombre d'itération
+ * @param n : le nombre d'itération
+ */
 void choisis_iteration(mpz_t k);
 
-void press_to_continue(void);
+/**
+ * Affiche un livre
+ */
+void livre(void);
 
-// fonction principale
+/**
+ * Fonction principale d'affichage et gestion entrée/sortie avec l'utilisateur
+ */
 void interface(void);
 
 #endif // !_interface_h
--- src/sandm.h
+++ src/sandm.h
@@ -1,13 +1,28 @@
 #ifndef _sandm_h
 #define _sandm_h
 
-// Compte le nombre de bit de l'entier h
+/**
+ * Compte le nombre de bit de l'entier h
+ * @param nb : résultat
+ * @param h : nombre (condition)
+ */
 void nb_bit(mpz_t nb, mpz_t h);
 
-// Calcul 2^i
+/**
+ * Calcul 2^i
+ * @param test : résultat
+ * @param i : nombre (condition)
+ */
 void pos(mpz_t test, mpz_t i);
 
 // res = a^h mod n
+/**
+ * Calcul res = a^h mod n
+ * @param res : résultat
+ * @param a : nombre (condition)
+ * @param n : nombre (condition) (module)
+ * @param h : nombre (condition) (exposant)
+ */
 void square_and_multiply(mpz_t res, mpz_t a, mpz_t n, mpz_t h);
 
 #endif // !_sandm_h
